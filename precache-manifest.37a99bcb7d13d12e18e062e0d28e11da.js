466f98b967e29cd03f5e8d8c7a26305e51f3a3a6
mowgli77 <bartend.it13@gmail.com>
2020-07-23 13:42:26 +0300

    Updates

---
+++ precache-manifest.37a99bcb7d13d12e18e062e0d28e11da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a91cab9ccd13b1a47a4e93a00aa4f21",
+    "url": "/bitmedia-front/index.html"
+  },
+  {
+    "revision": "7786d812bd1bd50d413a",
+    "url": "/bitmedia-front/static/css/main.a0b83bad.chunk.css"
+  },
+  {
+    "revision": "bec7d0f217cb5034bb22",
+    "url": "/bitmedia-front/static/js/2.c6a6143a.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/bitmedia-front/static/js/2.c6a6143a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7786d812bd1bd50d413a",
+    "url": "/bitmedia-front/static/js/main.ad9f175e.chunk.js"
+  },
+  {
+    "revision": "5e63d783ec4e27026209",
+    "url": "/bitmedia-front/static/js/runtime-main.8b57a99d.js"
+  },
+  {
+    "revision": "43cbfb652d4e5e5c48d23574329f4760",
+    "url": "/bitmedia-front/static/media/clean.43cbfb65.svg"
+  },
+  {
+    "revision": "42185a597bc71a3ecbb1b07e042efad9",
+    "url": "/bitmedia-front/static/media/footer.42185a59.svg"
+  },
+  {
+    "revision": "d32b47ac19870544dac2f33706562efe",
+    "url": "/bitmedia-front/static/media/header.d32b47ac.svg"
+  },
+  {
+    "revision": "62f0322d159d80939117b6140602b680",
+    "url": "/bitmedia-front/static/media/phone_img.62f0322d.svg"
+  },
+  {
+    "revision": "bfd64388e1a32cf784bf5ca07ce602f9",
+    "url": "/bitmedia-front/static/media/retina.bfd64388.svg"
+  },
+  {
+    "revision": "74d5a06e0f6b7028afe5617ff6cf0459",
+    "url": "/bitmedia-front/static/media/secure.74d5a06e.svg"
+  }
+]);
